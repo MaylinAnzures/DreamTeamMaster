b3a075360327001a2fd70df365e38523c93e5eaa
MitziSavna <savnamtz@gmail.com>
2024-11-05 19:08:59 -0600

    Otro

--- HealthySmile/src/componentes/EducacionDental.jsx
+++ HealthySmile/src/componentes/EducacionDental.jsx
@@ -20,49 +20,49 @@ export default function EducacionDental() {
                 </div>
                 <div className="gallery-item">
                     <img src="Caries.png" alt="Caries Dental" />
-                    <a href="cariesdentales.html" target="_blank" rel="noopener noreferrer">
+                    <a href="/Caries" target="_blank" rel="noopener noreferrer">
                         <p>Caries Dental</p>
                     </a>
                 </div>
                 <div className="gallery-item">
                     <img src="Cáncer.jpg" alt="Cáncer bucal" />
-                    <a href="cancerbucal.html" target="_blank" rel="noopener noreferrer">
+                    <a href="/Cancer" target="_blank" rel="noopener noreferrer">
                         <p>Cáncer bucal</p>
                     </a>
                 </div>
                 <div className="gallery-item">
                     <img src="traumatismo.jpg" alt="Traumatismos bucodentales" />
-                    <a href="traumatismo.html" target="_blank" rel="noopener noreferrer">
+                    <a href="/Traumatismo" target="_blank" rel="noopener noreferrer">
                         <p>Traumatismos bucodentales</p>
                     </a>
                 </div>
                 <div className="gallery-item">
                     <img src="Halitosis.jpg" alt="Halitosis" />
-                    <a href="halitosis.html" target="_blank" rel="noopener noreferrer">
+                    <a href="/Halitosis" target="_blank" rel="noopener noreferrer">
                         <p>Halitosis</p>
                     </a>
                 </div>
                 <div className="gallery-item">
                     <img src="sensibilidad.jpg" alt="Sensibilidad dental" />
-                    <a href="sensibilidaddental.html" target="_blank" rel="noopener noreferrer">
+                    <a href="/Sensibilidad" target="_blank" rel="noopener noreferrer">
                         <p>Sensibilidad dental</p>
                     </a>
                 </div>
                 <div className="gallery-item">
                     <img src="implantes.jpg" alt="Implantes" />
-                    <a href="implantes.html" target="_blank" rel="noopener noreferrer">
+                    <a href="/Implantes" target="_blank" rel="noopener noreferrer">
                         <p>Implantes</p>
                     </a>
                 </div>
                 <div className="gallery-item">
                     <img src="Periodontitis.jpg" alt="Periodontitis" />
-                    <a href="periodontitis.html" target="_blank" rel="noopener noreferrer">
+                    <a href="/Periodontitis" target="_blank" rel="noopener noreferrer">
                         <p>Periodontitis</p>
                     </a>
                 </div>
                 <div className="gallery-item">
                     <img src="quistes.png" alt="Quistes bucales" />
-                    <a href="quistesbucales.html" target="_blank" rel="noopener noreferrer">
+                    <a href="/Quiste" target="_blank" rel="noopener noreferrer">
                         <p>Quistes bucales</p>
                     </a>
                 </div>
