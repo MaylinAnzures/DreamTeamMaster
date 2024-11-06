c9abeee6957835e8770dc2f1cd72e8b0324f01fb
yael_a17 <144142451+yaela17@users.noreply.github.com>
2024-11-06 10:38:39 -0600

    funciona

--- HealthySmile/src/App.jsx
+++ HealthySmile/src/App.jsx
@@ -1,13 +1,12 @@
 import './App.css';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-import Index from './index';
 import ImplementacionChat from './Inicio/ImplementacionChat';
 import Consulta from './Inicio/Consulta';
 import Chatboti from './Inicio/Chatbot';
 import NotFound from './Inicio/NotFound';
 import IniciarSesion from './componentes/IniciarSesion.jsx';
-import { BrowserRouter as  Routes, Route, Navigate } from 'react-router-dom'; 
+import { Routes, Route, Navigate } from 'react-router-dom';
 import AyudaYSoporte from './componentes/AyudaYSoporte.jsx';
 import RegistrarSesion from './componentes/RegistrarSesion.jsx';
 import CodigoVerificacion from './componentes/CodigoVerificacion.jsx';
@@ -24,52 +23,29 @@ import Quiste from './componentes/Quiste.jsx';
 import Traumatismo from './componentes/Traumatismo.jsx';
 import Home from './componentes/Home.jsx';
 import Chat from './Inicio/Chat.jsx';
-import { useUserContext } from '../src/componentes/UserContext.jsx';  // Asegúrate de importar el contexto
+import { useUserContext } from '../src/componentes/UserContext.jsx';
 import Decision from './componentes/Decision.jsx';
 import DiagnosisChatbot from './Inicio/Diagnostico.jsx';
 import Chatbot from './Inicio/Chatbot';
 import ChatbotIntro from './Inicio/Chatbotito.jsx';
 
 function App() {
-  // Consumir el estado de estaLogueado desde el contexto
   const { estaLogueado } = useUserContext();
 
   return (
     <div className="app-container">
       <Routes>
-        <Route 
-          path="/" 
-          element={<Navigate to={estaLogueado ? "/Inicio" : "/Decision"} />} 
-        />
+        <Route path="/" element={<Navigate to={estaLogueado ? "/Inicio" : "/Decision"} />} />
         <Route path="/ImplementacionChat" element={<ImplementacionChat />} />
         <Route path="/Consulta" element={<Consulta />} />
         <Route path="/Chatbot" element={<Chatboti />} />
-        <Route path='/Chat' element={<Chat/>} />
+        <Route path="/Chat" element={<Chat />} />
         <Route path="*" element={<NotFound />} />
         <Route path="/IniciarSesion" element={<IniciarSesion />} />
         <Route path="/AyudaYSoporte" element={<AyudaYSoporte />} />
         <Route path="/Registro" element={<RegistrarSesion />} />
         <Route path="/Verificacion" element={<CodigoVerificacion />} />
-        <Route path='/IniciarSesion' element={<IniciarSesion/>}/>
-        <Route path='/AyudaYSoporte' element={<AyudaYSoporte/>}/>
-        <Route path='/Registro' element={<RegistrarSesion/>}/>
-        <Route path='/Verificacion' element={<CodigoVerificacion/>}/>
-        <Route path='/Home' element={<Inicio/>}/>
-        <Route path='/EducacionDental' element={<EducacionDental/>}/>
-        <Route path='/Gingivitis' element={<Gingivitis/>}/>
-        <Route path='/Cancer' element={<Cancer/>}/>
-        <Route path='/Caries' element={<Caries/>}/>
-        <Route path='/Halitosis' element={<Halitosis/>}/>
-        <Route path='/Implantes' element={<Implantes/>}/>
-        <Route path='/Periodontitis' element={<Periodontitis/>}/>
-        <Route path='/Sensibilidad' element={<Sensibilidad/>}/>
-        <Route path='/Quiste' element={<Quiste/>}/>
-        <Route path='/Traumatismo' element={<Traumatismo/>}/>
-        <Route path='/Diagnostico' element={<DiagnosisChatbot/>}/>
-        <Route path='/Chatbot' element={<Chatbot/>}/>
-        <Route path='/Chatbotito' element={<ChatbotIntro/>}/>
-        <Route path='/Inicio' element={<Home/>}/>
-        <Route path="/Home" element={<Inicio />} />
+        <Route path="/Inicio" element={<Home />} />
         <Route path="/EducacionDental" element={<EducacionDental />} />
         <Route path="/Gingivitis" element={<Gingivitis />} />
         <Route path="/Cancer" element={<Cancer />} />
@@ -80,8 +56,10 @@ function App() {
         <Route path="/Sensibilidad" element={<Sensibilidad />} />
         <Route path="/Quiste" element={<Quiste />} />
         <Route path="/Traumatismo" element={<Traumatismo />} />
-        <Route path="/Inicio" element={<Home />} />
-        <Route path='/Decision' element={<Decision/>}/>
+        <Route path="/Diagnostico" element={<DiagnosisChatbot />} />
+        <Route path="/Chatbot" element={<Chatbot />} />
+        <Route path="/Chatbotito" element={<ChatbotIntro />} />
+        <Route path="/Decision" element={<Decision />} />
       </Routes>
     </div>
   );
